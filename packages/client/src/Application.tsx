f70a4f5f02cafa0b6775621c86e8105a09090d78
Hardik Chauhan <hardik.chauhan@opensesame.com>
2024-04-14 18:33:15 +0530

    MAIN ORM Changes

---
+++ packages/client/src/Application.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Application: React.FC = () => (
+  <div className="App">
+    <header className="App-header"></header>
+      <p>
+        <h1>Hello, Mono - Repository CRM</h1>
+      </p>
+  </div>
+);
+
+export default Application;
